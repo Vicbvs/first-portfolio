61f97725e85c1cd54e2137326c8f12b2537c8834
Victor Borsatto <victor.borsattobh@gmail.com>
2022-03-25 14:54:22 -0300

    Changing the Prismic request

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,13 +1,10 @@
 import * as prismic from '@prismicio/client';
 
 export function getPrismicClient(req?: unknown) {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
-  client.enableAutoPreviewsFromReq(req);
+  const endpoint = prismic.getEndpoint(process.env.PRISMIC_REPO_NAME);
+  const client = prismic.createClient(endpoint);
 
-  // const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT, {
-  //   req,
-  //   accessToken: process.env.PRISMIC_ACCESS_TOKEN
-  // });
+  client.enableAutoPreviewsFromReq(req);
 
   return client;
 }
